7b5bf0633b1ffe777cb7abda9816c2b80b89a74f
anishmohan@gmail.com <anishmohan@gmail.com>
2017-02-15 07:17:18 -0800

    HW5

--- Lecture5/HW5_temp.R
+++ Lecture5/HW5_temp.R
@@ -59,14 +59,113 @@ mean.boot.auto.gas.price = one.boot(auto.gas.log.price, mean, R = 100000)
 mean.boot.auto.diesel.price = one.boot(auto.diesel.log.price, mean, R = 100000)
 plot.hist(mean.boot.auto.gas.price$t, mean.boot.auto.diesel.price$t, c(" Bootstrapping Auto Price: Fuel Type = Gas", " Bootstrapping Auto Price: Fuel Type = Diesel "))
 
-print(paste0("Mean value of Auto Price with fuel = Gas given by Bootstrap",mean.boot.auto.gas.price$t0))
-print(paste0("Mean value of Auto Price with fuel = Diesel given by Bootstrap",mean.boot.auto.diesel.price$t0))
-
-
+print(paste0("Mean value of Auto Price with fuel = Gas given by Bootstrap: $",mean.boot.auto.gas.price$t0))
+print(paste0("Mean value of Auto Price with fuel = Diesel given by Bootstrap: $",mean.boot.auto.diesel.price$t0))
 
 
 #Compare the differences of the bootstrap resampled mean of the log price of the autos grouped by body style. 
 #You will need to do this pair wise; e.g. between each possible pairing of body styles. Use both numerical and graphical methods 
 #for your comparison. Which pairs of means are different within a 95% confidence interval? How do your conclusions compare to the 
 #results you obtained from the ANOVA and Tukey’s HSD analysis you performed last week?
+
+auto = read.csv("Auto.csv", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+numcols = c("price","bore","stroke","horsepower","peak.rpm")
+auto[, numcols] <- lapply(auto[, numcols], as.numeric)
+auto = auto[complete.cases(auto),]
+
+boxplot(price~num.of.doors, data = auto)
+auto.door.aov = aov(price ~ as.factor(num.of.doors), data = auto)
+summary(auto.door.aov)
+print(auto.door.aov)
+tukey.door.anova = TukeyHSD(auto.door.aov)
+tukey.door.anova
+plot(tukey.door.anova)
+
+
+boxplot(price~body.style, data = auto)
+auto.body.aov = aov(price ~ as.factor(body.style), data = auto)
+summary(auto.body.aov)
+print(auto.body.aov)
+tukey.body.anova = TukeyHSD(auto.body.aov)
+tukey.body.anova
+plot(tukey.body.anova)
+
+auto.twoDoor.price = log(auto[auto$num.of.doors=="two",]$price)
+auto.fourDoor.price = log(auto[auto$num.of.doors == "four",]$price)
+
+
+#Bootstrap
+mean.boot.auto.twoDoor.price = one.boot(auto.twoDoor.price, mean, R = 100000)
+mean.boot.auto.fourDoor.price = one.boot(auto.fourDoor.price, mean, R = 100000)
+plot.hist(mean.boot.auto.twoDoor.price$t, mean.boot.auto.fourDoor.price$t, c(" Bootstrapping Auto Price: Number of Doors = 2", " Bootstrapping Auto Price: Number of Doors = 4 "))
+
+print(paste0("Mean value of Auto Price with number of Doors = 2; given by Bootstrap: $",mean.boot.auto.twoDoor.price$t0))
+print(paste0("Mean value of Auto Price with number of doors = 4; Door given by Bootstrap: $",mean.boot.auto.fourDoor.price$t0))
+
+auto.convertible.price = log(auto[auto$body.style =="convertible",]$price)
+auto.hardtop.price = log(auto[auto$body.style =="hardtop",]$price)
+auto.hatchback.price = log(auto[auto$body.style =="hatchback",]$price)
+auto.sedan.price = log(auto[auto$body.style =="sedan",]$price)
+auto.wagon.price = log(auto[auto$body.style =="wagon",]$price)
+
+#Bootstrap
+mean.boot.auto.convertible.price = one.boot(auto.convertible.price, mean, R = 100000)
+mean.boot.auto.hardtop.price = one.boot(auto.hardtop.price, mean, R = 100000)
+mean.boot.auto.hatchback.price = one.boot(auto.hatchback.price, mean, R = 100000)
+mean.boot.auto.sedan.price = one.boot(auto.sedan.price, mean, R = 100000)
+mean.boot.auto.wagon.price = one.boot(auto.wagon.price, mean, R = 100000)
+
+#Convertible vs Hardtop
+plot.hist(mean.boot.auto.convertible.price$t, mean.boot.auto.hardtop.price$t, c(" Bootstrapping Auto Price: Body Style = Convertible", " Bootstrapping Auto Price: Body Style = Hard Top "))
+print(paste0("Mean value of Auto Price with body style = Convertible; given by Bootstrap: $",mean.boot.auto.convertible.price$t0))
+print(paste0("Mean value of Auto Price with body style = Hardtop given by Bootstrap: $",mean.boot.auto.hardtop.price$t0))
+
+#Convertible vs Hatchback
+plot.hist(mean.boot.auto.convertible.price$t, mean.boot.auto.hatchback.price$t, c(" Bootstrapping Auto Price: Body Style = Convertible", " Bootstrapping Auto Price: Body Style = Hatchback "))
+print(paste0("Mean value of Auto Price with body style = Convertible; given by Bootstrap: $",mean.boot.auto.convertible.price$t0))
+print(paste0("Mean value of Auto Price with body style = Hatchback; given by Bootstrap: $",mean.boot.auto.hatchback.price$t0))
+
+# Convertible vs Sedan
+plot.hist(mean.boot.auto.convertible.price$t, mean.boot.auto.sedan.price$t, c(" Bootstrapping Auto Price: Body Style = Convertible", " Bootstrapping Auto Price: Body Style = Sedan "))
+print(paste0("Mean value of Auto Price with body style = Convertible; given by Bootstrap: $",mean.boot.auto.convertible.price$t0))
+print(paste0("Mean value of Auto Price with body style = Sedan; given by Bootstrap: $",mean.boot.auto.sedan.price$t0))
+
+# Convertible vs Wagon
+plot.hist(mean.boot.auto.convertible.price$t, mean.boot.auto.wagon.price$t, c(" Bootstrapping Auto Price: Body Style = Convertible", " Bootstrapping Auto Price: Body Style = Wagon "))
+print(paste0("Mean value of Auto Price with body style = Convertible; given by Bootstrap: $",mean.boot.auto.convertible.price$t0))
+print(paste0("Mean value of Auto Price with body style = Wagon; given by Bootstrap: $",mean.boot.auto.wagon.price$t0))
+
+
+#Hatchback vs Hardtop
+plot.hist(mean.boot.auto.hatchback.price$t, mean.boot.auto.hardtop.price$t, c(" Bootstrapping Auto Price: Body Style = Hatchback", " Bootstrapping Auto Price: Body Style = Hard Top "))
+print(paste0("Mean value of Auto Price with body style = Hatchback; given by Bootstrap: $",mean.boot.auto.hatchback.price$t0))
+print(paste0("Mean value of Auto Price with body style = Hardtop; given by Bootstrap: $",mean.boot.auto.hardtop.price$t0))
+
+
+# Sedan vs Hardtop
+plot.hist(mean.boot.auto.sedan.price$t, mean.boot.auto.hardtop.price$t, c(" Bootstrapping Auto Price: Body Style = Sedan", " Bootstrapping Auto Price: Body Style = Hard Top "))
+print(paste0("Mean value of Auto Price with body style = Sedan; given by Bootstrap: $",mean.boot.auto.sedan.price$t0))
+print(paste0("Mean value of Auto Price with body style = Hardtop; given by Bootstrap: $",mean.boot.auto.hardtop.price$t0))
+
+
+# Wagon vs Hardtop
+plot.hist(mean.boot.auto.wagon.price$t, mean.boot.auto.hardtop.price$t, c(" Bootstrapping Auto Price: Body Style = Wagon", " Bootstrapping Auto Price: Body Style = Hard Top "))
+print(paste0("Mean value of Auto Price with body style = Wagon; given by Bootstrap: $",mean.boot.auto.wagon.price$t0))
+print(paste0("Mean value of Auto Price with body style = Hardtop; given by Bootstrap: $",mean.boot.auto.hardtop.price$t0))
+
+
+#Hatchback vs Sedan
+plot.hist(mean.boot.auto.hatchback.price$t, mean.boot.auto.sedan.price$t, c(" Bootstrapping Auto Price: Body Style = Hatchback", " Bootstrapping Auto Price: Body Style = Sedan "))
+print(paste0("Mean value of Auto Price with body style = Hatchback; given by Bootstrap: $",mean.boot.auto.hatchback.price$t0))
+print(paste0("Mean value of Auto Price with body style = Sedan; given by Bootstrap: $",mean.boot.auto.sedan.price$t0))
+
+#Hatchback vs Wagon
+plot.hist(mean.boot.auto.hatchback.price$t, mean.boot.auto.wagon.price$t, c(" Bootstrapping Auto Price: Body Style = Hatchback", " Bootstrapping Auto Price: Body Style = Wagon "))
+print(paste0("Mean value of Auto Price with body style = Hatchback; given by Bootstrap: $",mean.boot.auto.hatchback.price$t0))
+print(paste0("Mean value of Auto Price with body style = Wagon; given by Bootstrap: $",mean.boot.auto.wagon.price$t0))
+
+#Sedan vs Wagon
+plot.hist(mean.boot.auto.sedan.price$t, mean.boot.auto.wagon.price$t, c(" Bootstrapping Auto Price: Body Style = Sedan", " Bootstrapping Auto Price: Body Style = Wagon "))
+print(paste0("Mean value of Auto Price with body style = Sedan; given by Bootstrap: $",mean.boot.auto.sedan.price$t0))
+print(paste0("Mean value of Auto Price with body style = Wagon; given by Bootstrap: $",mean.boot.auto.wagon.price$t0))
 
