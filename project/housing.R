a698dfae5b5a1e77dde2efae1b01738efad2dcb2
anishmohan@gmail.com <anishmohan@gmail.com>
2017-03-14 06:28:22 -0700

    proj:

--- project/housing.R
+++ project/housing.R
@@ -1,18 +1,24 @@
 library(ggplot2)
 library(reshape2)
+library(MASS)
 house = read.csv("train.csv", header = TRUE, stringsAsFactors = FALSE)
 house[complete.cases(house),]
 names(house)
 dim(house)
 str(house)
 
-house.int = house[,c("MSSubClass","LotFrontage","LotArea","OverallQual","OverallCond",
+todrop = c("Fence", "MiscFeature","PoolQC","GarageType","GarageQual","GarageCond", "GarageFinish","GarageYrBlt",
+           "FireplaceQu", "BsmtFinType1", "BsmtFinType2", "BsmtExposure","BsmtQual","BsmtCond","Alley","LotFrontage")
+house[,todrop] = NULL
+house = na.omit(house)
+
+house.int = house[,c("MSSubClass","LotArea","OverallQual","OverallCond",
                       "MasVnrArea","BsmtFinSF1","BsmtFinSF2","BsmtUnfSF","TotalBsmtSF",
                      "X1stFlrSF","X2ndFlrSF","LowQualFinSF","GrLivArea","BsmtFullBath",
                      "BsmtHalfBath","FullBath","HalfBath","BedroomAbvGr","KitchenAbvGr",
-                     "TotRmsAbvGrd","Fireplaces","GarageYrBlt","GarageCars","GarageArea",
+                     "TotRmsAbvGrd","Fireplaces","GarageCars","GarageArea",
                      "WoodDeckSF","OpenPorchSF","EnclosedPorch","X3SsnPorch","PoolArea",
-                     "MiscVal","MoSold","YrSold","SalePrice","LotArea")]
+                     "MiscVal","MoSold","YrSold","SalePrice")]
 
 summary(house.int)
 
@@ -32,8 +38,12 @@ cor(house.int)
 #[71] "ScreenPorch"   "PoolArea"      "PoolQC"        "Fence"         "MiscFeature"   "MiscVal"       "MoSold"       
 #3[78] "YrSold"        "SaleType"      "SaleCondition" "SalePrice" 
 
+#todrop = c("Fence", "MiscFeature","PoolQC","GarageType","GarageQual","GarageCond", "GarageFinish","GarageYrBlt",
+#           "FireplaceQu", "BsmtFinType1", "BsmtFinType2", "BsmtExposure","BsmtQual","BsmtCond","Alley","LotFrontage")
+#hist(house$LotFrontage)
+#hist(house$GarageYrBlt)
+
 hist(house$MSSubClass)
-hist(house$LotFrontage)
 hist(house$LotArea)
 hist(house$OverallQual)
 hist(house$OverallCond)
@@ -54,7 +64,7 @@ hist(house$BedroomAbvGr)
 hist(house$KitchenAbvGr)
 hist(house$TotRmsAbvGrd)
 hist(house$Fireplaces)
-hist(house$GarageYrBlt)
+
 hist(house$GarageCars)
 hist(house$GarageArea)
 hist(house$WoodDeckSF)
@@ -69,13 +79,34 @@ hist(house$SalePrice)
 hist(house$LotArea)
 #Exploratory Data Analyis
 
+#Removed ones:
+##todrop = c("Fence", "MiscFeature","PoolQC","GarageType","GarageQual","GarageCond", "GarageFinish","GarageYrBlt",
+#           "FireplaceQu", "BsmtFinType1", "BsmtFinType2", "BsmtExposure","BsmtQual","BsmtCond","Alley","LotFrontage")
+#ggplot(house, aes(y = SalePrice, x = LotArea)) + geom_point() + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
+#ggplot(house, aes(y = SalePrice, x = Alley)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = BsmtQual)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = BsmtCond)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = BsmtExposure)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = BsmtFinType1)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = BsmtFinType2)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = FireplaceQu)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = GarageType)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = GarageYrBlt)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = GarageFinish)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = GarageQual)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = GarageCond)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = PoolQC)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = Fence)) + geom_point()
+#ggplot(house, aes(y = SalePrice, x = MiscFeature)) + geom_point()
+
+
+
+
 ggplot(house, aes(y = SalePrice, x = MSSubClass)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = MSZoning)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = LotFrontage)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = LotArea)) + geom_point() + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
 ggplot(house, aes(y = log(SalePrice), x = log(LotArea))) + geom_point() + xlab("Log of Lot Area") + ylab("Log of Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
 ggplot(house, aes(y = SalePrice, x = Street)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = Alley)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = LotShape)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = LandContour)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = Utilities)) + geom_point()
@@ -101,11 +132,6 @@ ggplot(house, aes(y = SalePrice, x = MasVnrArea)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = ExterQual)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = ExterCond)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = Foundation)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = BsmtQual)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = BsmtCond)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = BsmtExposure)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = BsmtFinType1)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = BsmtFinType2)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = BsmtFinSF1)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = BsmtFinSF2)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = BsmtUnfSF)) + geom_point()
@@ -132,15 +158,10 @@ ggplot(house, aes(y = SalePrice, x = TotRmsAbvGrd)) + geom_point() #INdicator
 ggplot(house, aes(y = SalePrice, x = Functional)) + geom_point()
 
 ggplot(house, aes(y = SalePrice, x = Fireplaces)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = FireplaceQu)) + geom_point()
 
-ggplot(house, aes(y = SalePrice, x = GarageType)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = GarageYrBlt)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = GarageFinish)) + geom_point()
-gplot(house, aes(y = SalePrice, x = GarageCars)) + geom_point() #indicator
+
+ggplot(house, aes(y = SalePrice, x = GarageCars)) + geom_point() #indicator
 ggplot(house, aes(y = SalePrice, x = GarageArea)) + geom_point() #Indicator
-ggplot(house, aes(y = SalePrice, x = GarageQual)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = GarageCond)) + geom_point()
 
 ggplot(house, aes(y = SalePrice, x = PavedDrive)) + geom_point() #I
 
@@ -151,11 +172,6 @@ ggplot(house, aes(y = SalePrice, x = X3SsnPorch)) + geom_point()
 ggplot(house, aes(y = SalePrice, x = ScreenPorch)) + geom_point()
 
 ggplot(house, aes(y = SalePrice, x = PoolArea)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = PoolQC)) + geom_point()
-
-ggplot(house, aes(y = SalePrice, x = Fence)) + geom_point()
-ggplot(house, aes(y = SalePrice, x = MiscFeature)) + geom_point()
-
 ggplot(house, aes(y = SalePrice, x = MiscVal)) + geom_point()
 
 ggplot(house, aes(y = SalePrice, x = MoSold)) + geom_point()
@@ -268,5 +284,205 @@ ggplot(house, aes(x = factor(MiscFeature), y = SalePrice)) + geom_boxplot()
 ggplot(house, aes(x = factor(SaleType), y = SalePrice)) + geom_boxplot() #Look
 ggplot(house, aes(x = factor(SaleCondition), y = SalePrice)) + geom_boxplot() #Look
 
+#Violin Plots:
+ggplot(house, aes(x = factor(ExterCond), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
+ggplot(house, aes(x = factor(BsmtQual), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
+ggplot(house, aes(x = factor(BsmtCond), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
+ggplot(house, aes(x = factor(BsmtExposure), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Worth look
+ggplot(house, aes(x = factor(Heating), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #worth look
+ggplot(house, aes(x = factor(CentralAir), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Useless
+ggplot(house, aes(x = factor(Electrical), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #loook
+ggplot(house, aes(x = factor(KitchenQual), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #look
+ggplot(house, aes(x = factor(Functional), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) # worth look
+ggplot(house, aes(x = factor(FireplaceQu), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #worth look
+ggplot(house, aes(x = factor(GarageType), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #worth look
+ggplot(house, aes(x = factor(GarageFinish), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
+ggplot(house, aes(x = factor(GarageQual), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #look
+ggplot(house, aes(x = factor(GarageCond), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #worth look
+ggplot(house, aes(x = factor(PoolQC), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
+ggplot(house, aes(x = factor(MiscFeature), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75))
+ggplot(house, aes(x = factor(SaleType), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
+ggplot(house, aes(x = factor(SaleCondition), y = SalePrice)) + geom_violin(trim = T, draw_quantiles = c(0.25,0.5,0.75)) #Look
 
+
+
+#Hexbins:
+ggplot(house, aes(y = SalePrice, x = LotArea)) + stat_binhex(bins = 10)
+ggplot(house, aes(y = SalePrice, x = OverallQual)) + stat_binhex(bins = 10) #Good indicator for price
+ggplot(house, aes(y = SalePrice, x = TotalBsmtSF)) + stat_binhex(bins = 10) #Bsmt is a good indicator of price
+ggplot(house, aes(y = SalePrice, x = CentralAir)) + stat_binhex(bins = 10) #Impacts price
+ggplot(house, aes(y = SalePrice, x = X1stFlrSF)) + stat_binhex(bins = 10) #Good Indicator
+ggplot(house, aes(y = SalePrice, x = GrLivArea)) + stat_binhex(bins = 10) #Good inicator
+ggplot(house, aes(y = SalePrice, x = FullBath)) + stat_binhex(bins = 10) #Indicator
+
+#Multiple Dimensions:
+ggplot(house, aes(y = SalePrice, x = log(LotArea))) + geom_point(aes(color= factor(MSZoning))) + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
+ggplot(house, aes(y = SalePrice, x = log(LotArea))) + geom_point(aes(color= factor(LandContour))) + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price") #outliers
+ggplot(house, aes(y = SalePrice, x = log(LotArea))) + geom_point(aes(color= factor(BldgType))) + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
+
+
+ggplot(house, aes(y = SalePrice, x = log(TotalBsmtSF))) + geom_point(aes(color= factor(BsmtQual))) + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
+ggplot(house, aes(y = SalePrice, x = log(TotalBsmtSF))) + geom_point(aes(color= factor(BsmtFinType1))) + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
+
+ggplot(house, aes(y = SalePrice, x = (CentralAir))) + geom_point(aes(color= factor(BsmtFinType2))) + xlab("Lot Area") + ylab("Sale Price") + ggtitle("Relationship between Lot Area and Sale Price")
+
+
+#Regression:
+house$logSalePrice = log(house$SalePrice)
+
+lm.house = lm(logSalePrice ~.-SalePrice,data = house)
+summary(lm.house)
+plot(lm.house)
+
+lm.house.aic = stepAIC(lm.house, direction = "both")
+lm.house.aic$anova
+
+
+house.svd = house
+house.svd$SalePrice = NULL
+cols = names(house.int)
+cols = cols[cols!="SalePrice"]
+cols = c(cols, "logSalePrice")
+house.scale = house.svd
+house.scale[,cols] = lapply(house.svd[,cols], function(x) { scale(x, center = TRUE, scale = TRUE)})
+house.modmat = model.matrix(logSalePrice~., data = house.scale)
+
+
+plot.svd.reg <- function(df, k){
+  require(ggplot2)
+  require(gridExtra)
+  
+  p1 <- ggplot(df) + 
+    geom_point(aes(score, resids), size = 2) + 
+    stat_smooth(aes(score, resids)) +
+    ggtitle('Residuals vs. fitted values')
+  
+  p2 <- ggplot(df, aes(resids)) +
+    geom_histogram(aes(y = ..density..)) +
+    geom_density(color = 'red', fill = 'red', alpha = 0.2) +
+    ggtitle('Histogram of residuals')
+  
+  qqnorm(df$resids)
+  
+  grid.arrange(p1, p2, ncol = 2)
+  
+  df$std.resids = sqrt((df$resids - mean(df$resids))^2)  
+  
+  p3 = ggplot(df) + 
+    geom_point(aes(score, std.resids), size = 2) + 
+    stat_smooth(aes(score, std.resids)) +
+    ggtitle('Standardized residuals vs. fitted values')
+  print(p3) 
+  
+  n = nrow(df)
+  Ybar = mean(df$logSalePrice)
+  SST <- sum((df$logSalePrice - Ybar)^2)
+  SSR <- sum(df$resids * df$resids)
+  SSE = SST - SSR
+  RMSE = sqrt(SSR/(n-2))
+  cat(paste('SSE =', as.character(SSE), '\n'))
+  cat(paste('SSR =', as.character(SSR), '\n'))
+  cat(paste('SST =', as.character(SSE + SSR), '\n'))
+  cat(paste('RMSE =', as.character(SSE/(n - 2)), '\n'))
+  
+  adjR2  <- 1.0 - (SSR/SST) * ((n - 1)/(n - k - 1))
+  cat(paste('Adjusted R^2 =', as.character(adjR2)), '\n')
+}
+
+
+
+M = house.modmat[,-1]
+M2 = t(M)%*%M
+head(M2)
+
+mSVD = svd(M2)
+plot(log(mSVD$d))
+
+inv.diag = rep(0, length(mSVD$d))
+inv.diag[1:190] = 1/mSVD$d[1:190]
+mD = diag(inv.diag)
+
+#Inverse (t(M)*M) = V Diag U
+mInV = mSVD$v %*% mD %*% t(mSVD$u)
+b = house.scale$logSalePrice
+#x = Inv(MTM)*t(M)*b
+x = mInV %*% t(M) %*% b
+
+x = ginv(M) %*% b
+house.cp = house
+house.cp$score =  (M %*% x)*sd(house.cp$logSalePrice) + mean(house.cp$logSalePrice)
+house.cp$resids =  house.cp$score - house.cp$logSalePrice
+
+RSS = sum((house.cp$logSalePrice - (house.cp$score))^2)
+RSS
+TSS = sum((house.cp$logSalePrice - mean(house.cp$logSalePrice))^2)
+TSS
+require(repr)
+options(repr.pmales.extlot.width=8, repr.plot.height=4)
+plot.svd.reg(house.cp, length(names(house))-1)
+
+#Elastic Net
+
+#Plotting function for elastitc net
+plot.elastic.reg <- function(df, k){
+  require(ggplot2)
+  require(gridExtra)
+  
+  p1 <- ggplot(df) + 
+    geom_point(aes(score, resids), size = 2) + 
+    stat_smooth(aes(score, resids)) +
+    ggtitle('Residuals vs. fitted values')
+  
+  p2 <- ggplot(df, aes(resids)) +
+    geom_histogram(aes(y = ..density..)) +
+    geom_density(color = 'red', fill = 'red', alpha = 0.2) +
+    ggtitle('Histogram of residuals')
+  
+  qqnorm(df$resids)
+  
+  grid.arrange(p1, p2, ncol = 2)
+  
+  df$std.resids = sqrt((df$resids - mean(df$resids))^2)  
+  
+  p3 = ggplot(df) + 
+    geom_point(aes(score, std.resids), size = 2) + 
+    stat_smooth(aes(score, std.resids)) +
+    ggtitle('Standardized residuals vs. fitted values')
+  print(p3) 
+  
+  n = nrow(df)
+  Ybar = mean(df$logSalePrice)
+  SST <- sum((df$logSalePrice - Ybar)^2)
+  SSR <- sum(df$resids * df$resids)
+  SSE = SST - SSR
+  RMSE = sqrt(SSR/(n-2))
+  cat(paste('SSE =', as.character(SSE), '\n'))
+  cat(paste('SSR =', as.character(SSR), '\n'))
+  cat(paste('SST =', as.character(SSE + SSR), '\n'))
+  cat(paste('RMSE =', as.character(RMSE), '\n'))
+  
+  adjR2  <- 1.0 - (SSR/SST) * ((n - 1)/(n - k - 1))
+  cat(paste('Adjusted R^2 =', as.character(adjR2)), '\n')
+}
+
+
+library(glmnet)
+#M = ModelMatrix
+#auto.cp$price = log price
+#Specifying the dependent variable and the features
+M = house.modmat[,-1]
+b = house.cp$logSalePrice
+
+#Runnnign ridge + Lasso
+mod.ridge.lasso = glmnet(M, b, family = 'gaussian', nlambda = 20, alpha = 0.5)
+
+#Storing the predictions in the Auto data frame
+house.cp$score = predict(mod.ridge.lasso, newx = M)[, 15]
+house.cp$resids = house.cp$score - house.cp$logSalePrice
+
+#Plotting the decay of variables based on lambda value
+plot(mod.ridge.lasso, xvar = 'lambda', label = TRUE)
+plot(mod.ridge.lasso, xvar = 'dev', label = TRUE)
+
+plot.elastic.reg(house.cp, length(names(house))-1)
 
